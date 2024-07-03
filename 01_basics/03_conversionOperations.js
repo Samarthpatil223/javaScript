fb025a50ed07cfd430ed0a376a539a7025b6e36b
Samarth Patil <141984698+Samarthpatil223@users.noreply.github.com>
2024-07-03 15:48:50 +0000

    done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,83 @@
+// let score=33
+// console.log(typeof score);//number
+
+//let score="33"
+//console.log(typeof score);//string
+
+// let valueInNumber=Number(score)//conversion operation
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//33
+
+// let score="33abc"
+// let valueInNumber=Number(score)//conversion operation
+// console.log(typeof valueInNumber);//number(it shows it is a number but thats not a number)
+// console.log(valueInNumber);//NAN(Not a Number)
+//  true => 1; false => 0
+
+// let isLoggedIn=1;
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)//true
+
+
+// let someNumber=33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);//33
+// console.log(typeof stringNumber);//string
+
+
+
+//Operations
+
+// let value=3
+// let negValue=-value
+// console.log(negValue);//-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);//2 to the power 3
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Hello"
+// let str2 = " Samarth"
+// let str3 = str1 + str2
+// console.log(str3);//Hello Samarth
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+
+
+// console.log(+true);//1
+// console.log(+"");//0
+
+// let gameCounter = 100
+// //++gameCounter;//101
+// gameCounter++;//101
+// console.log(gameCounter);
+
+
+//Comparison
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);//true
+// console.log("02" > 1);//true
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
